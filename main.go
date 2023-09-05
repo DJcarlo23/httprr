d42580c33c5d799fdbe8e9ed3aca82024129eb04
DJcarlo23 <krystian.wicherek89@gmail.com>
2023-09-05 08:22:25 +0200

    "Working prototype"

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
+}
 
 func main() {
-	fmt.Println("hello world")
+	ulrFlag := flag.String("u", "url", "a url of the target website")
+	flag.Parse()
+
+	if flagStatus := isFlagPassed("u"); flagStatus == true {
+		resp, err := http.Get(*ulrFlag)
+
+		if err != nil {
+			fmt.Print(errors.New("Invalid request"))
+		}
+
+		for key, element := range resp.Header {
+			fmt.Printf("%s: %v \n", key, element[0])
+		}
+
+	}
+
 }
